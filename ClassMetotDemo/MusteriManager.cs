84931308a436b93fd38fb56672a7f6400f326ba8
BurakMihrioglu <burakmihrioglu@gmail.com>
2021-01-21 23:10:51 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Yeni Müşteri Sisteme Kaydedildi!\n\n" + "Müşteri ID: " + musteri.Id + "\n" + "Müşteri Adı: " + musteri.Ad + "\n" + "Müşteri Soyadı: " + musteri.Soyad + "\n" + "Müşteri Telefon Numarası: " + musteri.TelNo + "\nMüşteri Doğum Tarihi: " + musteri.DogumTarihi + "\n" +  "\n");
+        }
+
+        public void Cikar(Musteri musteri)
+        {
+            Console.WriteLine("Aşağıda Belirtilen Müşteri Sistemden Çıkartılmıştır!\n\n" + "Müşteri ID: " + musteri.Id + "\n" + "Müşteri Adı: " + musteri.Ad + "\n" + "Müşteri Soyadı: " + musteri.Soyad + "\n" + "Müşteri Telefon Numarası: " + musteri.TelNo + "\nMüşteri Doğum Tarihi: " + musteri.DogumTarihi + "\n" + "\n");
+        }
+
+        public void Listele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri Bilgileri:\n\n" + "Müşteri ID: " + musteri.Id + "\n" + "Müşteri Adı: " + musteri.Ad + "\n" + "Müşteri Soyadı: " + musteri.Soyad + "\n" + "Müşteri Telefon Numarası: " + musteri.TelNo + "\nMüşteri Doğum Tarihi: " + musteri.DogumTarihi + "\n" + "\n");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Burak";
+            musteri1.Soyad = "Mihrioğlu";
+            musteri1.TelNo = "1111111111111";
+            musteri1.DogumTarihi = 999999;
+            
+
+
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Engin";
+            musteri2.Soyad = "Demiroğ";
+            musteri2.TelNo = "22222222";
+            musteri2.DogumTarihi = 8888;
+            
+
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            Console.WriteLine("YENİ MÜŞTERİ EKLEME METODU \n");
+
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+            Console.WriteLine("MÜŞTERİYİ SİSTEMDEN SİLME METODU \n");
+
+            musteriManager.Cikar(musteri1);
+            musteriManager.Cikar(musteri2);
+
+            Console.WriteLine("MÜŞTERİ BİLGİLERİNİ LİSTELEME METODU \n");
+
+            musteriManager.Listele(musteriler);
+
+        }
+    }
+}
